b5c3a9a0262588bd27cff4da9f9afa9a0df2e8cf
phucnguyen1998 <phucnguyen11011998@gmail.com>
2020-05-05 09:21:19 +0700

    add layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <ul className="nav navbar-nav">
+              <li className="nav-item active">
+                  <a className="nav-link">CALL API</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link">Trang chủ</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link">Quản lý sản phẩm</a>
+              </li>
+          </ul>
+      </nav>
+
+      <div className="container-fluid">
+        <h1 className="display-4 my-4 text-info">List of products</h1>
+        <button type="button" className="btn btn-primary mb-1">Thêm sản phẩm</button>
+        <table className="table table-striped" id="users" style={{width: '100%'}}>
+          <thead>
+            <tr id="list-header">
+              <th scope="col">ID</th>
+              <th scope="col">Mã Sản Phẩm</th>
+              <th scope="col">Tên Sản Phẩm</th>
+              <th scope="col">Giá</th>
+              <th scope="col">Trạng thái</th>
+              <th scope="col">Hành động</th>
+            </tr>
+          </thead>  
+          <tbody>  
+            <tr>
+              <td>1</td>
+              <td>SS</td>
+              <td>SS</td>
+              <td>123321</td>
+              <td>
+                <span className="badge badge-info">
+                  còn hàng
+                </span>
+              </td>
+              <td>
+                <button type="button" className="btn btn-success">Sửa</button>
+                <button type="button" className="btn btn-danger">Xóa</button>
+              </td>
+            </tr>    
+          </tbody>
+        </table>
+        
+      </div>
     </div>
   );
 }
